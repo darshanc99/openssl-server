407286355e3d134f3d54be94391994acb82e72f8
Darshan Chheda <darshandvc@gmail.com>
2020-11-08 23:43:42 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,81 @@
+import os
+
+def generatersa():
+    name = input("Enter the applicant's name: ")
+    id = input("Enter the unique ID: ")
+    name = name + id
+    if os.path.isdir(name):
+        print("Sorry,",name,"cannot generate new Keypairs. It already has been assigned one.")
+    else:
+        os.system('mkdir ' + name)
+        os.system('mkdir ' + name + '/sent')
+        os.system('mkdir ' + name + '/received')
+        priv = 'openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -pkeyopt rsa_keygen_pubexp:3 -out ' + name + '/' + name + 'priv.pem'
+        os.system(priv)
+        pub = 'openssl pkey -in ' + name + '/' + name + 'priv.pem' + ' -out ' + name + '/' + name + 'pub.pem -pubout'
+        os.system(pub)
+        print("User added to the system and keys generated.")
+
+def sendmessages():
+    sender = input("Enter your username: ")
+    if os.path.isdir(sender):
+        receiver = input("Enter the receiver's username: ")
+        if os.path.isdir(receiver):
+            message = input("Enter your secret message: ")
+            filename = input("What do you name the message file: ")
+
+            path = sender + '/sent/' + filename + ".txt"
+            f = open(path,"w")
+            f.write(message)
+            f.close()
+
+            operatin = 'openssl pkeyutl -encrypt -in ' + path + ' -pubin -inkey ' + receiver + '/' + receiver + 'pub.pem ' + '-out server/' + filename + '.bin'
+            os.system(operatin)
+            print("\nYour message is sent to the server.")
+        else:
+            print("No such username exists. Try again!")
+    else:
+        print("No such username exists. Try again!")
+
+def receivemessages():
+    receiver = input("Enter your username: ")
+    if os.path.isdir(receiver):
+        os.system('ls server/')
+        print()
+        filename = input("Which file do you want to access: ")
+        newpath = 'server/' + filename
+        if os.path.isfile(newpath):
+            operation = 'openssl pkeyutl -decrypt -in ' + newpath + ' -inkey ' + receiver + '/' + receiver + 'priv.pem' + ' -out ' + receiver + '/received/' + filename[:-4] + '.txt'
+            #print(operation)
+            os.system(operation)
+            newpath = receiver + '/received/' + filename[:-4] + '.txt'
+            if os.path.isfile(newpath):
+                print("You can now view this file chat!")
+                operation = 'rm ' + receiver + '/received/' + filename[:-4] + '.txt'
+                os.system(operation)
+            else:
+                print("That file chat is not for you. Access denied.")
+        else:
+            print("No such file exists. Try again!")
+    else:
+        print("No such username exists. Try again!")
+
+while (True):
+    print("\nWhat do you want to do?")
+    print("1: Generate RSA Keypair.")
+    print("2: Send Messages.")
+    print("3: Receive Messages.")
+    print("4: Exit.")
+    choice = int(input("Enter the choice: "))
+
+    if choice == 1:
+        generatersa()
+    elif choice == 2:
+        sendmessages()
+    elif choice == 3:
+        receivemessages()
+    elif choice == 4:
+        print("Exiting the System. Thank You!")
+        break
+    else:
+        print("You entered a wrong choice. Please re-enter!")
